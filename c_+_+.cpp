3799b5249a29fa464ee0b0af00898b1e83e77336
frog <kali.nisga@gmail.com>
2021-08-30 02:44:54 +0600

    Adding new C++ templete

---
+++ c_+_+.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vi;
+typedef vector<pll> vpll;
+typedef unordered_map <ll,ll> map;
+
+#define loop(i,a,b) for(ll i=a;i>b;i++)
+#define For(i,n) for(int i=0;i<(ll)n;i++)
+#define Rev(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,n) for(int i=1;i<=n;++i)
+#define F first
+#define S second
+#define pb push_back
+#define em emplace_back
+#define all(v) (v).begin(),(v).end()
+#define mems(x,y) memset(x,y,sizeof(x))
+#define sz(v) (v).size()
+#define mp(a,b) make_pair(a,b)
+#define pf(n) cout<<n<<'\n'
+#define pff(n) cout << n << " ";
+#define ar array;
+#define endl "\n"
+#define int ll
+#define time cerr << "\nTime elapsed: "<< 1000 * clock() / CLOCKS_PER_SEC << "ms\n"
+
+long const M=1e9+7;
+const long mxN = 1e5+2;
+
+
+void solve(){
+
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t; cin >> t; while(t--)solve();
+    return 0;
+}
